b5acc36928c79d36d0891a1c96a7327fb61570f6
ctshuk <57163286+ctshuk@users.noreply.github.com>
2023-10-10 11:42:06 +0800

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,26 +1,6 @@
-input.onButtonPressed(Button.A, function () {
-    password.move(1)
-})
-input.onButtonPressed(Button.AB, function () {
-    if (password.get(LedSpriteProperty.X) == 2 && password.get(LedSpriteProperty.Y) == 2) {
-        basic.showIcon(IconNames.Yes)
-        mode = 1
-    } else {
-        basic.showIcon(IconNames.No)
-    }
-})
-input.onButtonPressed(Button.B, function () {
-    password.turn(Direction.Right, 90)
-})
-let password: game.LedSprite = null
-let mode = 0
-mode = 0
-let point1 = game.createSprite(2, 2)
-point1.set(LedSpriteProperty.Brightness, 0)
-password = game.createSprite(3, 0)
-password.set(LedSpriteProperty.Brightness, 100)
-basic.forever(function () {
-    if (mode == 1) {
+radio.onReceivedNumber(function (receivedNumber) {
+    if (receivedNumber == 1) {
         edubitTrafficLightBit.setLed(LedColor.All, edubitTrafficLightBit.digitalStatePicker(DigitalIoState.On))
     }
 })
+radio.setGroup(1)
